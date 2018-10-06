82fa60476d4667033a0298068cc6cf53706dd76b
luisaneininger <luisaneininger@luisas-mbp.local>
2018-10-06 19:52:42 +0200

    Aufgabe0

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -1,1 +1,22 @@
+/**
+Aufgabe: 0
+Name: Luisa Neininger
+Matrikel: 259314
+Datum: 06.10.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+**/
+var Aufgabe0;
+(function (Aufgabe0) {
+    var name = "";
+    function box() {
+        var name = prompt("Gebe deinen Namen ein:");
+        var node = document.getElementById("Inhalt");
+        node.innerHTML += "Hallo ";
+        node.innerHTML += name;
+        node.innerHTML += ", schoen dich zu sehen! ";
+        console.log("Hallo", name, ", schoen dich zu sehen!");
+    }
+    document.addEventListener('DOMContentLoaded', box);
+})(Aufgabe0 || (Aufgabe0 = {}));
 //# sourceMappingURL=Aufgabe0.js.map
---
+++ Aufgabe0/Aufgabe0.ts
@@ -0,0 +1,30 @@
+/**
+Aufgabe: 0
+Name: Luisa Neininger
+Matrikel: 259314 
+Datum: 06.10.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+**/
+
+namespace Aufgabe0
+
+{
+    
+    var name : string ="";
+    function box(){
+        var name = prompt ("Gebe deinen Namen ein:");
+        var node : any= document.getElementById("Inhalt");
+        
+        node.innerHTML += "Hallo ";
+        node.innerHTML += name;
+        node.innerHTML += ", schoen dich zu sehen! ";
+        
+        console.log("Hallo",name,", schoen dich zu sehen!")
+        
+       }
+        
+document.addEventListener('DOMContentLoaded',box);
+}
+
+
