6b6986d95c570d37e0f4f2051d529b66d1a02085
luisaneininger <luisaneininger@luisas-mbp.local>
2018-10-21 13:27:24 +0200

    fertig Aufgabe 2

--- Aufgabe 2/Aufgabe2.ts
+++ Aufgabe 2/Aufgabe2.ts
@@ -3,7 +3,7 @@ namespace Aufgabe2 {
 
 
     function game() {
-//Interface von allen Karten und jeweils ihre Anzahl 
+        //Interface von allen Karten und jeweils ihre Anzahl 
         interface Cards {
             card: string;
             red: number;
@@ -154,39 +154,44 @@ namespace Aufgabe2 {
 
 
 
-//Zusammenfügung von allen Karten in einem einzigen Array
+        //Zusammenfügung von allen Karten in einem einzigen Array
         let AllCards: Cards[] = [cardzero, cardone, cardtwo, cardthree, cardfour, cardfive, cardsix, cardseven,
             cardnine, cardplustwo, cardretour, cardtimeout, cardwish, cardplusfour]
 
 
-//Deklaration der Zufalls(random) Funktion damit später zufällig aus den Karten gewählt werden kann.
-        function Zufall(n: number) { return Math.floor(Math.random() * Math.floor(n)) };
+        //Deklaration der Zufalls(random) Funktion damit später zufällig aus den Karten gewählt werden kann.
+        function Zufall(Nummer: number) { return Math.floor(Math.random() * Math.floor(Nummer)) };
 
 
-//function placeDiv, erstellt Dokument in HTML, div ist kind von body
-        function placeDiv(_color: string, _x: number, _e: string): void {
+        //function placeDiv, erstellt Dokument in HTML, div ist kind von body
+        function placeDiv(_color: string, Karte: string, Eingabe: number): void {
             let div: HTMLDivElement = document.createElement("div");
             document.body.appendChild(div);
 
-            div.setAttribute("id", "a" + _x);
-            document.getElementById("a" + _x).innerHTML += _e;
-//CSS Style Deklaration
+            div.setAttribute("id", "a" + Eingabe);
+            document.getElementById("a" + Eingabe).innerHTML += Karte;
+            //CSS Style Deklaration
             let s: CSSStyleDeclaration = div.style;
-            s.border = "thin solid gray";
+            s.border = "solid black";
             s.position = "absolute";
             s.backgroundColor = _color;
             s.width = 150 + "px";
             s.height = 200 + "px";
-            s.left = (_x + 1) * 155 + "px";
-            s.top = 30 + "px";
+            s.left = (Eingabe + 1) * 160 + "px"; //Abstand zwischen den Spielkarten nach jeder Wiederholung
+            s.bottom = 50 + "px";
+            s.textAlign = "center";
+            s.borderRadius = 20 + "px";
 
 
 
-//Schriftfarbe wird festgelegt falls Hintergrund zu dunkel ist
+            //Schriftfarbe wird festgelegt falls Hintergrund zu dunkel ist
 
             if (_color == "black") { s.color = "white"; }
 
-            if (_color == "blue") { s.color = "white"; }
+            if (_color == "#2E9AFE") { s.color = "white"; }
+
+            if (_color == "#DF0101") { s.color = "white"; }
+
         }
 
 
@@ -195,10 +200,10 @@ namespace Aufgabe2 {
 
 
 
-//Pop up, spieler gibt ein mit wie vielen Karten er spielen möchte
-        let a: number;
-        let i: string = prompt("Gebe ein mit wie vielen Karten du spielen möchtest");
-        a = Number(i); //i wird in a zurückgegeben damit man eine zahl eingeben kann
+        //Pop up, spieler gibt ein mit wie vielen Karten er spielen möchte
+        let Anzahl: number;
+        let Kartenauswahl: string = prompt("Gebe ein mit wie vielen Karten du spielen moechtest");
+        Anzahl = Number(Kartenauswahl); //Kartenauswahl wird in Anzahl zurückgegeben damit man eine zahl eingeben kann
 
 
 
@@ -212,22 +217,22 @@ namespace Aufgabe2 {
 
         let c: string = "";
 
-        for (let d: number = 0; d < a; d++) { //wenn d kleiner ist als a dann wird bei jeder Wiederholung d plus 1 gerechnet!
+        for (let i : number = 0; i < Anzahl; i++) { //wenn d kleiner ist als a dann wird bei jeder Wiederholung d plus 1 gerechnet!
 
 
-            let z : number = Zufall(15); //Funktion oben wird aufgegriffen um zufällig aus den Karten zu wählen
+            let Kartenzahl: number = Zufall(15); //Funktion oben wird aufgegriffen um zufällig aus den Karten zu wählen
 
-            let h: number = Zufall(5);
+            let Farben: number = Zufall(5);
 
-            switch (h) {
+            switch (Farben) {
 
 
 
                 case 0:
-                    c = "red";
-                    if (AllCards[z].red > 0) {
-                        placeDiv(c, AllCards[z].card, d);
-                        AllCards[z].red--;
+                    c = "#DF0101";
+                    if (AllCards[Kartenzahl].red > 0) {
+                        placeDiv(c, AllCards[Kartenzahl].card, i);
+                        AllCards[Kartenzahl].red--;
                         continue;
                     }
 
@@ -235,10 +240,10 @@ namespace Aufgabe2 {
                 case 1:
 
 
-                    c = "green";
-                    if (AllCards[z].green > 0) {
-                        placeDiv(c, AllCards[z].card, d);
-                        AllCards[z].green--;
+                    c = "#81F79F";
+                    if (AllCards[Kartenzahl].green > 0) {
+                        placeDiv(c, AllCards[Kartenzahl].card, i);
+                        AllCards[Kartenzahl].green--;
                         continue;
                     }
 
@@ -249,10 +254,10 @@ namespace Aufgabe2 {
 
 
                 case 2:
-                    c = "blue";
-                    if (AllCards[z].blue > 0) {
-                        placeDiv(c, AllCards[z].card, d);
-                        AllCards[z].blue--;
+                    c = "#2E9AFE";
+                    if (AllCards[Kartenzahl].blue > 0) {
+                        placeDiv(c, AllCards[Kartenzahl].card, i);
+                        AllCards[Kartenzahl].blue--;
                         continue;
                     }
 
@@ -260,68 +265,71 @@ namespace Aufgabe2 {
 
 
                 case 3:
-                    c = "yellow";
-                    if (AllCards[z].yellow > 0) {
-                        placeDiv(c, AllCards[z].card, d);
-                        AllCards[z].yellow--;
+                    c = "#F3F781";
+                    if (AllCards[Kartenzahl].yellow > 0) {
+                        placeDiv(c, AllCards[Kartenzahl].card, i);
+                        AllCards[Kartenzahl].yellow--;
                         continue;
                     }
 
 
                 case 4:
                     c = "black";
-                    if (AllCards[z].black > 0) {
-                        placeDiv(c, AllCards[z].card, d);
-                        AllCards[z].black--;
+                    if (AllCards[Kartenzahl].black > 0) {
+                        placeDiv(c, AllCards[Kartenzahl].card, i);
+                        AllCards[Kartenzahl].black--;
                         continue;
                     }
 
                     else {
-                        d--;
+                        i--;
                         continue
                     }
             }
         }
-    
 
-//Stylesheet vom Stapel
-    function Stapel() {
-        let div = document.createElement("div");
-        document.body.appendChild(div);
-        div.setAttribute("id", "Unostapel");
-        document.getElementById("Kartenstapel").innerHTML += "Unostapel";
-        let s = div.style;
-        s.border = "thin solid black";
-        s.textAlign = "center";
-        s.position = "absolute";
-        s.backgroundColor = "gray";
-        s.width = 150 + "px";
-        s.height = 200 + "px";
-        s.left = 60 + "px";
-        s.top = 30 + "px";
+
+        //Stylesheet vom Stapel
+        function Stapel() {
+            let div = document.createElement("div");
+            document.body.appendChild(div);
+            div.setAttribute("id", "Unostapel");
+            document.getElementById("Unostapel").innerHTML += "Unostapel";
+            let s = div.style;
+            s.border = " solid black";
+            s.textAlign = "center";
+            s.position = "absolute";
+            s.backgroundColor = "#EFFBF2";
+            s.width = 150 + "px";
+            s.height = 200 + "px";
+            s.left = 550 + "px";
+            s.top = 20 + "px";
+            s.borderRadius = 20 + "px";
+        }
+        //Stylesheet der Ablage
+        function Ablage() {
+            let div = document.createElement("div");
+            document.body.appendChild(div);
+            div.setAttribute("id", "Kartenablage");
+            document.getElementById("Kartenablage").innerHTML += "Kartenablage";
+            let s = div.style;
+            s.border = "solid black";
+            s.textAlign = "center";
+            s.position = "absolute";
+            s.backgroundColor = "light gray";
+            s.width = 150 + "px";
+            s.height = 200 + "px";
+            s.right = 550 + "px";
+            s.top = 20 + "px";
+            s.borderRadius = 20 + "px";
+
+        }
+
+        Ablage();
+        Stapel();
+
     }
-//Stylesheet der Ablage
-    function Ablage() {
-        let div = document.createElement("div");
-        document.body.appendChild(div);
-        div.setAttribute("id", "Kartenablage");
-        document.getElementById("Kartenablage").innerHTML += "Kartenablage";
-        let s = div.style;
-        s.border = "solid black";
-        s.textAlign = "center";
-        s.position = "absolute";
-        s.backgroundColor = "light gray";
-        s.width = 150 + "px";
-        s.height = 200 + "px";
-        s.right = 50 + "px";
-        s.top = 20 + "px";
-    }
-
-    Ablage();
-    Stapel();
-
-}
-document.addEventListener("DOMContentLoaded", (game)) 
+    document.addEventListener("DOMContentLoaded", (game))
 
 
 }
