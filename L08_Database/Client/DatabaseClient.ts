a252fca34807f673bfd9cd471bbf6fca4e9eca5f
luisaneininger <luisaneininger@luisas-mbp.eduroam.hs-furtwangen.de>
2018-12-14 14:54:25 +0100

    wefwegf

--- L08_Database/Client/DatabaseClient.ts
+++ L08_Database/Client/DatabaseClient.ts
@@ -25,6 +25,15 @@ namespace DatabaseClient {
         let query: string = "command=refresh";
         sendRequest(query, handleFindResponse);
     }
+    
+    function find(_event: Event): void{
+        let search: HTMLInputElement = <HTMLInputElement>document.getElementById("number");
+        let query: string ="command=find";
+        query+= "&matrikel=" + search.value ;
+        console.log(query);
+        sendRequest(query, handleFindResponse);
+        }
+    
 
     function sendRequest(_query: string, _callback: EventListener): void {
         let xhr: XMLHttpRequest = new XMLHttpRequest();
